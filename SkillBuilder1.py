85b0b62480951d203f6b821024da62fc8033eca7
lifes30daytrial <jonathanxu29@hotmail.com>
2021-11-03 12:34:36 -0600

    skill builder 1

---
+++ SkillBuilder1.py
@@ -0,0 +1,30 @@
+# Skill Builder 1
+
+# Task 1: Print Hello Python World!
+print("-- Lines 3-5 --")
+print("Hello Python World!")
+
+# Task 2: Print "your name"
+print("-- Lines 7-9 --")
+print("Jonathan")
+
+# Task 3: Using a variable named message print Hello Python Universe
+print("-- Lines 11-14 --")
+greetings = "Hello Python Universe"
+print(greetings)
+
+# Task 4: Print "your name" using title methods
+print("-- Lines 16-19 --")
+name = "Jonathan"
+print(name.title())
+
+# Task 5: Print "your name" using lower method
+print("-- Lines 21-23 --")
+print(name.lower())
+
+# Task 6: Print "your name" using upper method
+print("-- Lines 25-27 --")
+print(name.upper())
+
+# Bonus: Print to console in color
+print("\033[93m ERROR: Something went right.\033[0m")
