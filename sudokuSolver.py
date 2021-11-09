da9290e71ee70346ee4f3d15f7a2c699592fa1ac
lifes30daytrial <jonathanxu29@hotmail.com>
2021-11-09 10:23:36 -0700

    sodoku finished

---
+++ sudokuSolver.py
@@ -0,0 +1,81 @@
+import numpy as np
+import time
+# sdkTable = np.zeros((9, 9), dtype=int)
+global sdkTable, solving, rowMissing, columnMissing, squareMissing
+sdkTable = np.array([[0, 0, 0, 2, 6, 0, 7, 0, 1],
+                    [6, 8, 0, 0, 7, 0, 0, 9, 0],
+                    [1, 9, 0, 0, 0, 4, 5, 0, 0],
+                    [8, 2, 0, 1, 0, 0, 0, 4, 0],
+                    [0, 0, 4, 6, 0, 2, 9, 0, 0],
+                    [0, 5, 0, 0, 0, 3, 0, 2, 8],
+                    [0, 0, 9, 3, 0, 0, 0, 7, 4],
+                    [0, 4, 0, 0, 5, 0, 0, 3, 6],
+                    [7, 0, 3, 0, 1, 8, 0, 0, 0]])
+numberTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+solving = True
+rowMissing = []
+columnMissing = []
+squareMissing = []
+
+
+def diff(l1, l2):
+    diffTable = [i for i in l1 + l2 if i not in l1 or i not in l2]
+    return diffTable
+
+
+def sim(l1, l2, l3):
+    tempL = []
+    for a in range(0, len(l1)):
+        for b in range(0, len(l2)):
+            for c in range(0, len(l3)):
+                if l1[a] == l2[b] == l3[c]:
+                    tempL.append(l1[a])
+    return tempL
+
+
+def findOne():
+    errCnt = 0
+    for x in range(0, 9):
+        for y in range(0, 9):
+            missing = sim(rowMissing[x], columnMissing[y], squareMissing[(x // 3 * 3) + y//3])
+            if len(missing) == 1:
+                if sdkTable[x, y] == 0:
+                    return x, y, missing[0]
+
+
+print(sdkTable)
+while solving:
+    # Analyze phase
+    rowMissing = []
+    columnMissing = []
+    squareMissing = []
+    for row in range(0, 9):  # Analyze rows
+        rowMissing.append(diff(list(sdkTable[row, 0:10]), numberTable))
+    for column in range(0, 9):  # Analyze columns
+        columnMissing.append(diff(list(sdkTable[0:10, column]), numberTable))
+    for squareX in range(0, 3):  # Analyze "squares" (3x3 sections)
+        for squareY in range(0, 3):
+            sSX1 = squareX * 3
+            sSX2 = squareX * 3 + 3
+            sSY1 = squareY * 3
+            sSY2 = squareY * 3 + 3
+            squareList = sdkTable[sSX1:sSX2, sSY1:sSY2]
+            squareMissing.append(diff(list(squareList.flatten()), numberTable))
+    if sdkTable[findOne()[0], findOne()[1]] == 0:
+        sdkTable[findOne()[0], findOne()[1]] = findOne()[2]  # sdkTable[x, y] = missing
+    try:
+        rowMissing[findOne()[0]].remove(findOne()[2])
+    except TypeError:
+        pass
+    if 0 not in sdkTable:
+        solving = False
+    print(sdkTable)
+print(sdkTable)
+
+
+"""print(f"Rows: {rowMissing}")
+print(f"Column: {columnMissing}")
+print(f"Squares: {squareMissing}")
+print(rowMissing[0], columnMissing[0], squareMissing[0])
+print(sim(rowMissing[0], columnMissing[1], squareMissing[0]))
+print(fillOne())"""
